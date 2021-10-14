527f02a4fd42b82ce8a6abff4481cdbedea4e607
kishan1996 <kishanmenaria321@gmail.com>
2021-10-14 22:34:22 +0530

    setup done

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kishan1996/winequality",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="kishanmenaria321@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[
